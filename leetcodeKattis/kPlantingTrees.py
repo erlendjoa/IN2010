4f98b9737e31415d06b3be33c625db11c9f1aa11
Erlend Joa <erlend.joa@gmail.com>
2023-08-24 18:01:56 +0200

    m

--- leetcodeKattis/kPlantingTrees.py
+++ leetcodeKattis/kPlantingTrees.py
@@ -4,11 +4,13 @@ nrOfSeeds = int(input())
 growthTimesInput = input().split(" ")
 growthTimes = sorted(growthTimesInput, reverse=True)
 
-dayTimer = 0
-print(growthTimes)
-for i in range(1, len(growthTimes)+1):
-    currentDay = int(growthTimes[i-1])
-    if currentDay+i >= dayTimer:
-        dayTimer = currentDay+i
 
-print(dayTimer)
+class Seed:
+    def __init__(self, n):
+        self.n = n
+    def grow(self):
+        self.n -= 1
+        if self.n == 0:
+            self = None
+
+f
