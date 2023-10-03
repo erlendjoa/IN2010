3199fc01ef272f50467fe606ca3a9829a1d12e11
Erlend Joa <erlend.joa@gmail.com>
2023-10-03 20:23:28 +0200

    m

--- Obliger/Oblig_2/Selvbalanser.py
+++ Obliger/Oblig_2/Selvbalanser.py
@@ -2,25 +2,30 @@ import math;
 
 def selvbalanser(L):
     A = []
-    mid = L.pop((len(L)-1)//2)
+    mid = L.pop((len(L))//2)
     A.append(mid)
     print(L, mid)
 
-    insertRec(L[(len(L)-1)//2:], A)
-    insertRec(L[:(len(L)-1)//2], A)
+    insertRec(L[math.ceil((len(L))/2):], A)
+    insertRec(L[:math.ceil((len(L))/2)], A)
     return A
-    
+
 
 def insertRec(L, A):
-    print(L, (len(L)-1)//2)
-    mid = L.pop((len(L)-1)//2)
+    if len(L) == 0:
+        return
+    mid = L.pop(len(L)//2)
     A.append(mid)
+    print(L, mid)
+ 
+    if (len(L) == 2):
+        A.append(L.pop(1))
+        A.append(L.pop(0))
+        print(L)
+    else:
+        insertRec(L[math.ceil((len(L))/2):], A)
+        insertRec(L[:math.ceil((len(L))/2)], A)
 
-    if len(L) > 1:
-        insertRec(L[(len(L)-1)//2:], A)
-        insertRec(L[:(len(L)-1)//2], A)
-    if len(L) == 1:
-        A.append(L[0])
 
-arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 print(selvbalanser(arr))
