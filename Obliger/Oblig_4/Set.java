8f308b1a9b22aa3feae52631f8b0c87ff274e618
Erlend Joa <erlend.joa@gmail.com>
2023-10-29 13:32:08 +0100

    m

--- Obliger/Oblig_4/Set.java
+++ Obliger/Oblig_4/Set.java
@@ -9,13 +9,25 @@ class Set {
     private int hash(String s, int N) {
         return s.length();
     }
-
+    /*
+    private int hash(String s, int N) {
+        int h = 0;
+        for (int i = 0; i < s.length(); i++) {
+            h = 31*h + (int) s.charAt(i);
+        }
+        return h%N;
+    }
+    */
     public void insert(String s) {
         set[hash(s, set.length)] = s;
     }
-    public String contains(int n)
+    public boolean contains(String s) {
+        return set[hash(s, set.length)] != null;
+    }
     
     public static void main(String[] args) {
-
+        Set s = new Set(10);
+        s.insert("hei");
+        System.out.println(s.contains("hei"));
     }
 }
