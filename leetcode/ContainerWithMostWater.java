e1a28bceeae9a6ab3c374c7d6dfa6278857bf9bf
Erlend Joa <erlend.joa@gmail.com>
2024-01-26 22:51:05 +0100

    m

---
+++ leetcode/ContainerWithMostWater.java
@@ -0,0 +1,16 @@
+
+
+class Container {
+
+    public static void main(String[] args) {
+
+        int[] height = new int[10];
+
+        for (int i = 0; i < height.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if 
+            }
+        }
+
+    }
+}
